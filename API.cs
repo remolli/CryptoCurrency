0754af466e70797a4e0ab054c0ae26240a9f79a4
Evandru <evandro_remolli.t@outlook.com>
2023-06-23 16:01:41 -0300

    add: GetAsset e GetAssets async functions

--- API.cs
+++ API.cs
@@ -18,5 +18,19 @@ namespace CryptoCurrency
             var requestDeserialized = await client.GetAsync<GetExchangeRateModel>(request);
             return requestDeserialized;
         }
+        public async Task<GetAssetModel[]> GetAssetsAsync()
+        {
+            var request = new RestRequest($"/v1/assets");
+            request.AddHeader("X-CoinAPI-Key", "11CA632B-F6B4-419E-8E88-65246C594C97");
+            var requestDeserialized = await client.GetAsync<GetAssetModel[]>(request);
+            return requestDeserialized;
+        }
+        public async Task<GetAssetModel> GetAssetAsync(string asset_id)
+        {
+            var request = new RestRequest($"/v1/assets/{asset_id}");
+            request.AddHeader("X-CoinAPI-Key", "11CA632B-F6B4-419E-8E88-65246C594C97");
+            var requestDeserialized = await client.GetAsync<GetAssetModel>(request);
+            return requestDeserialized;
+        }
     }
 }
