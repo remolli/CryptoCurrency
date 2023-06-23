8a3b236e05b33525d1b375dbcd74d718b2998ab9
Evandru <evandro_remolli.t@outlook.com>
2023-06-23 16:37:26 -0300

    fix: type of properties

--- Models/GetAssetModel.cs
+++ Models/GetAssetModel.cs
@@ -4,19 +4,19 @@
     {
         public string asset_id { get; set; }
         public string name { get; set; }
-        public string type_is_crypto { get; set; }
-        public string data_start { get; set; }
-        public string data_end { get; set; }
-        public string data_quote_start { get; set; }
-        public string data_quote_end { get; set; }
-        public string data_orderbook_start { get; set; }
-        public string data_orderbook_end { get; set; }
-        public string data_trade_start { get; set; }
-        public string data_trade_end { get; set; }
-        public string data_symbols_count { get; set; }
-        public string volume_1hrs_usd { get; set; }
-        public string volume_1day_usd { get; set; }
-        public string volume_1mth_usd { get; set; }
-        public string price_usd { get; set; }
+        public long type_is_crypto { get; set; }
+        public DateTimeOffset data_start { get; set; }
+        public DateTimeOffset data_end { get; set; }
+        public DateTimeOffset data_quote_start { get; set; }
+        public DateTimeOffset data_quote_end { get; set; }
+        public DateTimeOffset data_orderbook_start { get; set; }
+        public DateTimeOffset data_orderbook_end { get; set; }
+        public DateTimeOffset data_trade_start { get; set; }
+        public DateTimeOffset data_trade_end { get; set; }
+        public long data_symbols_count { get; set; }
+        public double volume_1hrs_usd { get; set; }
+        public double volume_1day_usd { get; set; }
+        public double volume_1mth_usd { get; set; }
+        public double price_usd { get; set; }
     }
 }
