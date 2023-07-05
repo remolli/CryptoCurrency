ad032a6fbc41f17ee56eb832fb31a1ad99a87fbc
Evandru <evandro_remolli.t@outlook.com>
2023-07-05 16:26:15 -0300

    modify: adaptation for new API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,43 +16,11 @@ namespace CryptoCurrency.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var responseDeserialized = await _api.GetTickersAsync();
-            return View(responseDeserialized);
-        }
-
-        public async Task<IActionResult> Ticker(string symbol)
-        {
-            if (symbol == null)
-            {
-                symbol = "BTC";
-            }
-            var responseDeserialized = await _api.GetTickerAsync(symbol);
-            return View(responseDeserialized);
-        }
-
-        public async Task<IActionResult> Tickers()
-        {
-            var responseDeserialized = await _api.GetTickersAsync();
+            var responseDeserialized = await _api.GetAssetsAsync();
             return View(responseDeserialized);
         }
 
         // ---------------------------------------
-
-        public async Task<IActionResult> Symbol(string symbol)
-        {
-            if (symbol == null)
-            {
-                symbol = "BTC";
-            }
-            var responseDeserialized = await _api.GetSymbolAsync(symbol);
-            return View(responseDeserialized);
-        }
-
-        public async Task<IActionResult> Symbols()
-        {
-            var responseDeserialized = await _api.GetSymbolsAsync();
-            return View(responseDeserialized);
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
