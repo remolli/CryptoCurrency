7f3ea853300003f27bf606a3a7155484f186c93b
Evandru <evandro_remolli.t@outlook.com>
2023-07-05 16:25:35 -0300

    add: new API

--- API.cs
+++ API.cs
@@ -8,35 +8,14 @@ namespace CryptoCurrency
         RestClient client;
         public API()
         {
-            var options = new RestClientOptions("https://api.blockchain.com/v3/exchange");
+            var options = new RestClientOptions("https://api.coincap.io/v2");
             client = new RestClient(options);
         }
-        public async Task<GetTickerModel[]> GetTickersAsync()
+        public async Task<GetAssetsModel> GetAssetsAsync()
         {
-            var request = new RestRequest($"/tickers");
-            request.AddHeader("X-API-Token", "7905f5e7-94c4-44f4-a229-d496e9c3d7a1");
-            var requestDeserialized = await client.GetAsync<GetTickerModel[]>(request);
-            return requestDeserialized;
-        }
-        public async Task<GetTickerModel> GetTickerAsync(string symbol)
-        {
-            var request = new RestRequest($"/tickers/{symbol}");
-            request.AddHeader("X-API-Token", "7905f5e7-94c4-44f4-a229-d496e9c3d7a1");
-            var requestDeserialized = await client.GetAsync<GetTickerModel>(request);
-            return requestDeserialized;
-        }
-        public async Task<Dictionary<string, GetSymbolModel>> GetSymbolsAsync()
-        {
-            var request = new RestRequest($"/symbols");
-            request.AddHeader("X-API-Token", "7905f5e7-94c4-44f4-a229-d496e9c3d7a1");
-            var requestDeserialized = await client.GetAsync<Dictionary<string, GetSymbolModel>>(request);
-            return requestDeserialized;
-        }
-        public async Task<GetSymbolModel> GetSymbolAsync(string symbol)
-        {
-            var request = new RestRequest($"/symbols/{symbol}");
-            request.AddHeader("X-API-Token", "7905f5e7-94c4-44f4-a229-d496e9c3d7a1");
-            var requestDeserialized = await client.GetAsync<GetSymbolModel>(request);
+            var request = new RestRequest($"/assets");
+            request.AddHeader("Authorization", "Bearer7b46f726-cc15-4d61-92c1-778ee01669d0");
+            var requestDeserialized = await client.GetAsync<GetAssetsModel>(request);
             return requestDeserialized;
         }
     }
